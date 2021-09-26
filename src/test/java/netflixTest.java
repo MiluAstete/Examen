be408e45b7e42aaefb2825869dc246f037114269
miluAstete <miluskaevy272@gmail.com>
2021-09-26 00:07:28 -0500

    actualizaciónLandingPage

--- src/test/java/netflixTest.java
+++ src/test/java/netflixTest.java
@@ -1,24 +1,7 @@
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.BeforeMethod;
 
-
-import java.util.concurrent.TimeUnit;
 
 public class netflixTest {
 
-    public WebDriver driver;
-    public String URL = "https://www.netflix.com/pe/";
-
-    @BeforeMethod
-    public void setUp() throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver","drivers/chromedriver.exe");
-        driver =  new ChromeDriver();
-        driver.get(URL);
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
-    }
 
 
 }
